69089bd3a7b6a4dfe2234aeedb35f36df048f4f3
heguanghao <roc_hgh@aliyun.com>
2015-05-12 17:30:11 +0800

    -屏蔽spine自带的播放动画逻辑代码

--- cocos/editor-support/spine/SkeletonRenderer.cpp
+++ cocos/editor-support/spine/SkeletonRenderer.cpp
@@ -75,7 +75,7 @@ void SkeletonRenderer::initialize () {
 	setOpacityModifyRGB(true);
 
 	setGLProgram(ShaderCache::getInstance()->getGLProgram(GLProgram::SHADER_NAME_POSITION_TEXTURE_COLOR));
-	scheduleUpdate();
+	//scheduleUpdate();
 }
 
 void SkeletonRenderer::setSkeletonData (spSkeletonData *skeletonData, bool ownsSkeletonData) {
